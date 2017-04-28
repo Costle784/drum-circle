fafa5e0136b7aad849f3169550d08ef4651bc93d
Curtis Ostle <curtisostle@gmail.com>
2017-04-27 21:07:01 -0400

    full crud on jamposts and comments

---
+++ app/controllers/comments_controller.rb
@@ -0,0 +1,33 @@
+class CommentsController < ApplicationController
+  #create
+  def create
+    @jampost = Jampost.find(params[:jampost_id])
+    @comment = @jampost.comments.create(comment_params)
+    redirect_to jampost_path(@jampost)
+  end
+  #edit
+  def edit
+    @jampost = Jampost.find(params[:jampost_id])
+    @comment = @jampost.comments.find(params[:id])
+  end
+  #update
+  def update
+    @jampost = Jampost.find(params[:jampost_id])
+    @comment = @jampost.comments.find(params[:id])
+    @comment.update(comment_params)
+    redirect_to jampost_path(@jampost)
+  end
+  #destroy
+  def destroy
+    @jampost = Jampost.find(params[:jampost_id])
+    @comment = @jampost.comments.find(params[:id])
+    @comment.destroy
+    redirect_to jampost_path(@jampost)
+  end
+
+  private
+
+  def comment_params
+    params.require(:comment).permit(:name, :body)
+  end
+end
--- app/controllers/jamposts_controller.rb
+++ app/controllers/jamposts_controller.rb
@@ -18,4 +18,27 @@ class JampostsController < ApplicationController
     @jampost = Jampost.find(params[:id])
     @comment = Comment.new
   end
+  #edit
+  def edit
+    @jampost = Jampost.find(params[:id])
+  end
+  #update
+  def update
+    @jampost = Jampost.find(params[:id])
+    @jampost.update(jampost_params)
+
+    redirect_to jampost_path(@jampost)
+  end
+
+  def destroy
+    @jampost = Jampost.find(params[:id])
+    @jampost.destroy
+
+    redirect_to jamposts_path
+  end
+
+  private
+  def jampost_params
+    params.require(:jampost).permit(:title, :img_url, :location, :body)
+  end
 end
